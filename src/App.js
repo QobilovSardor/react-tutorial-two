799163ed57a338a68403725972c310719f868927
SardorDev <sardorqobilov555@gmail.com>
2023-01-23 23:47:05 +0500

    function components project, project name of counter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import React, { useState } from 'react';
+
 function App() {
+  const [count, setCount] = useState(0);
+
+  function increment() {
+    setCount(count + 2)
+  }
+
+  function decrement() {
+    setCount(count - 1)
+  }
+
+  function reset() {
+    setCount(0)
+  }
+
   return (
-    <div className="App">
-      <h1>Salom dunyo</h1>
+    <div className="card">
+      <h1 className="card-title text-center">Counter: {count}</h1>
+      <div className="card-body d-flex justify-content-between">
+        <button className="btn btn-info" onClick={increment}>Increment +</button>
+        <button className="btn btn-secondary" onClick={decrement}>Decrement -</button>
+        <button className="btn btn-danger" onClick={reset}>Reset 0</button>
+      </div>
     </div>
   );
 }
